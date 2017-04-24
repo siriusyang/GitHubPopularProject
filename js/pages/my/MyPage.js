ca44e2752673e9f9f9311573613d673325cbb040
siriusyang <304501989@qq.com>
2017-04-24 16:53:44 +0800

    优化我的

--- js/pages/my/MyPage.js
+++ js/pages/my/MyPage.js
@@ -17,18 +17,9 @@ import NavigationBar from "../../component/NavigationBar"
 import CustomKeyPage from "./CustomKeyPage"
 import SortPage from "./SortPage"
 import Consts from "../utils/Consts"
+const CUSTOM_PAGE = 'CustomKeyPage';
+const SORT_PAGE = 'CustomKeyPage';
 export default class MyPage extends Component {
-    static defaultProps = {
-        items: [{
-            'name': '自定义分类',
-            'image': '../../../res/images/ic_custom_language.png',
-            'onPress': this.goToCustomKeyPag
-        }, {
-            'name': '分类排序',
-            'image': '../../../res/images/ic_swap_vert.png', 'onPress': this.sortPage
-        }]
-    }
-
     goToCustomKeyPage = () => {
         this.props.navigator.push({
             title: CustomKeyPage
@@ -39,6 +30,41 @@ export default class MyPage extends Component {
             title: SortPage
         });
     }
+    static defaultProps = {
+        items: [{
+            'name': '自定义分类',
+            'image': require('../../../res/images/ic_custom_language.png'),
+            'press': CUSTOM_PAGE,
+        }, {
+            'name': '分类排序',
+            'image': require('../../../res/images/ic_swap_vert.png'),
+            'press': SORT_PAGE
+        }]
+    }
+    press = (type) => {
+        if (type === SORT_PAGE) {
+            this.sortPage();
+        } else if (type === CUSTOM_PAGE) {
+            this.goToCustomKeyPage();
+        }
+    }
+
+    rendItem = (item, i) => {
+        return < View style={styles.items} key={`key${i}`}>
+            <TouchableOpacity activeOpacity={0.7} onPress={() => this.press(item.press)} style={{flex: 1, height: 40}}>
+                <View style={styles.welcome}>
+
+                    <Image source={item.image}
+                           style={{width: 22, height: 22, tintColor: '#63B8FF'}}/>
+                    <Text style={{marginLeft: 5, flex: 1}}>
+                        {item.name}
+                    </Text>
+                    <Image source={require('../../../res/images/ic_tiaozhuan.png')}
+                           style={{width: 22, height: 22, tintColor: '#63B8FF'}}/>
+                </View>
+            </TouchableOpacity>
+        </View>
+    }
 
     render() {
         return (
@@ -47,34 +73,10 @@ export default class MyPage extends Component {
                     title={Consts.MY}
                 />
                 <ScrollView style={{flex: 1}}>
-                    < View style={styles.items}>
-                        <TouchableOpacity activeOpacity={0.7} onPress={this.goToCustomKeyPage} style={{flex: 1,   height:40}}>
-                            <View style={styles.welcome}>
+                    {this.props.items.map((item, i) => {
+                        return this.rendItem(item, i);
+                    })}
 
-                                <Image source={require('../../../res/images/ic_custom_language.png')}
-                                       style={{width: 22, height: 22, tintColor: '#63B8FF'}}/>
-                                <Text style={{marginLeft: 5, flex: 1}}>
-                                    {'自定义分类'}
-                                </Text>
-                                <Image source={require('../../../res/images/ic_tiaozhuan.png')}
-                                       style={{width: 22, height: 22, tintColor: '#63B8FF'}}/>
-                            </View>
-                        </TouchableOpacity>
-                    </View>
-                    < View style={styles.items}>
-                        <TouchableOpacity activeOpacity={0.7} onPress={this.sortPage} style={{flex: 1,   height:40,}}>
-                            <View style={styles.welcome}>
-
-                                <Image source={require('../../../res/images/ic_swap_vert.png')}
-                                       style={{width: 22, height: 22, tintColor: '#63B8FF'}}/>
-                                <Text style={{marginLeft: 5, flex: 1}}>
-                                    {'分类排序'}
-                                </Text>
-                                <Image source={require('../../../res/images/ic_tiaozhuan.png')}
-                                       style={{width: 22, height: 22, tintColor: '#63B8FF'}}/>
-                            </View>
-                        </TouchableOpacity>
-                    </View>
                 </ScrollView>
             </View>
         );
@@ -82,30 +84,30 @@ export default class MyPage extends Component {
 }
 
 const styles = StyleSheet.create({
-        container: {
-            flex: 1,
-        },
-        items: {
-            flexDirection: 'row',
-            backgroundColor: '#FFF',
-            marginLeft: 5,
-            marginRight: 5,
-            paddingLeft:5,
-            marginVertical: 5,
-            borderColor: '#dddddd',
-            borderWidth: 0.5,
-            borderRadius: 2,
-            shadowColor: 'gray',
-            shadowOffset: {width: 0.5, height: 0.5},
-            shadowRadius: 1, //阴影半径
-            shadowOpacity: 0.4,
-            elevation: 2 //Android 投影
-        },
-        welcome: {
-            flexDirection: 'row',
-            flex: 1,
-            alignItems: 'center',
+    container: {
+        flex: 1,
+    },
+    items: {
+        flexDirection: 'row',
+        backgroundColor: '#FFF',
+        marginLeft: 5,
+        marginRight: 5,
+        paddingLeft: 5,
+        marginVertical: 5,
+        borderColor: '#dddddd',
+        borderWidth: 0.5,
+        borderRadius: 2,
+        shadowColor: 'gray',
+        shadowOffset: {width: 0.5, height: 0.5},
+        shadowRadius: 1, //阴影半径
+        shadowOpacity: 0.4,
+        elevation: 2 //Android 投影
+    },
+    welcome: {
+        flexDirection: 'row',
+        flex: 1,
+        alignItems: 'center',
 
-        },
+    },
 
-    });
+});
